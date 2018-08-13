d269ebcedd71ca4e7734cd69c0280dbbdebedd57
Tiago Catita <tiago.catita@ericsson.com>
2018-08-13 16:35:08 +0200

    Demo scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from '@eds/angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
