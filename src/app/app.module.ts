b1dc748ec1110b82f615bd27140c87e2ab0b0cda
Tiago Catita <tiago.catita@ericsson.com>
2018-09-03 15:58:32 +0200

    Removed CUSTOM_ELEMENTS_SCHEMA from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -48,7 +48,6 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     TreeModule
   ],
   providers: [ThemeService],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
