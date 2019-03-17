11e2d1890678d6b2eca20d946402af48d43aca1a
Lutfi Aldi Nugroho <aldinugroho@ezaldlu.id>
2019-03-17 21:32:31 +0700

    int

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TicketService } from './ticket.service';
@@ -22,6 +22,8 @@ import { NgxPaginationModule } from "ngx-pagination";
 import { RouterModule, Routes } from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbdCarouselBasic } from './carousel-basic';
+
 
 
 import {
@@ -61,6 +63,7 @@ import { ActivityService } from './activity.service';
     // AccordionModule,
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppBarModule,
     ButtonModule,
     DropdownModule,
